904a832b5e0c2f101c1a66517353431e7c5aba64
Mark Bell <mark@swoop.com>
2017-12-11 16:05:33 -0500

    Initial Implementation

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='graphouse-graphite-api',
+    version='0.1.1',
+    url='https://github.com/swoop-inc/graphouse-graphite-api',
+    license='MIT',
+    author='Mark Bell',
+    author_email='mark@swoop.com',
+    description='Graphouse storage adaptor for graphite-api',
+    packages=find_packages('.'),
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'six'
+    ]
+)
